568821925663e1d01d04faa835e5d84dd229f893
nrogachev <nrogachev@gmail.com>
2024-12-06 08:39:50 +0100

    day 6 part 2, working test

--- day06/day06.py
+++ day06/day06.py
@@ -24,16 +24,29 @@ def process_input(lines):
 
     pos = start
     dir = map[start[0]][start[1]]
-    map[start[0]][start[1]] = 'X'
     visited = set([start])
-    visited_with_dir = set([(start, dir)])
-    obstacles = set()
     while True:
         next_move = (pos[0] + directions[dir][0], pos[1] + directions[dir][1])
         if not (0 <= next_move[0] < rows and 0 <= next_move[1] < cols):
             break
+        if map[next_move[0]][next_move[1]] == '#':
+            # hit a wall, turn right
+            dir = rotation_order[(rotation_order.index(dir) + 1) % 4]
+            continue
+        else:   
+            pos = next_move
+            visited.add(pos)
+    # for row in range(rows):
+    #     print(''.join(map[row]))
+    part1 = len(visited)
 
-        if (next_move, dir) in visited_with_dir:
+    pos = start
+    dir = map[start[0]][start[1]]
+    obstacles = set()
+    visited_with_dir = set([(start, dir)])
+    while True:
+        next_move = (pos[0] + directions[dir][0], pos[1] + directions[dir][1])
+        if not (0 <= next_move[0] < rows and 0 <= next_move[1] < cols):
             break
         o_dir = rotation_order[(rotation_order.index(dir) + 1) % 4]
         if map[next_move[0]][next_move[1]] == '#':
@@ -51,14 +64,9 @@ def process_input(lines):
                 if not (0 <= o_next_move[0] < rows and 0 <= o_next_move[1] < cols):
                     break
             pos = next_move
-            # map[pos[0]][pos[1]] = 'X'
-            visited.add(pos)
             visited_with_dir.add((pos, dir))
-    # for row in range(rows):
-    #     print(''.join(map[row]))
-    part1 = len(visited)
 
-    # print(obstacles)
+    print(obstacles)
     part2 = len(obstacles)
     return part1, part2
 
